ff3b1e01ce9a52427e5d72e3620ae2a996cd0020
MatiasRodriguez12 <matiasrodriguez552@gmail.com>
2023-04-20 23:17:58 -0300

    Se agregan archivos de TP 4

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include "gpio.h"
+
+int main(void){
+	
+	gpio_t led_rojo=gpioCreate(1,6,true);
+	gpio_t tecla=gpioCreate(1,7,false);
+	
+	while(true){
+		if(gpioGetValue(tecla)==0){
+			gpioSetValue(led_rojo, true);
+			}
+		else{
+			gpioSetValue(led_rojo, false);
+			}
+		}
+	return 0;
+	}
